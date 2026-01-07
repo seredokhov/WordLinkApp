dd3c45144b8c5cae1a122b14fdc05c50a95a1bec
Pavel <seredokhovp@mail.ru>
2026-01-07 03:49:49 +0300

    Initial commit

--- axios.js
+++ axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: 'http://192.168.0.149:3000', // local
-     baseURL: 'http://192.168.0.149/api', // docker
-//    baseURL: 'http://85.209.90.167:4000', // remote
+    baseURL: 'url/api',
     headers: {
         "Access-Control-Allow-Origin": "*"
     }
